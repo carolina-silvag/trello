3bd7dd89a69889a0172dda266c7627dc0980bbe4
carolina-silvag <carolinasguzman@gmail.com>
2017-11-08 12:17:09 -0300

    version 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,23 @@
 /*aparecer formulario*/
 document.getElementById("lista").onclick = function(){
 	document.getElementById('lista').className = "oculto"
-	console.log('hola')
 	document.getElementById('proyecto').className = "visible"
 }
+/*desaparesca formulario al presinar X*/
 document.getElementById("times").onclick = function(){
 	document.getElementById("proyecto").className = "oculto"
 	document.getElementById("lista").className = "visible"
 }
+/*agregar el texto al presionar guardar*/
+document.getElementById("crearLista").onclick = function(){
+	var nameListas= document.getElementById('nameLista').value;
+	document.getElementById('nameLista').value = '';
+	var newListas= document.createElement('div');
+	var cont = document.getElementById('cont');
+	var paragraph= document.createElement('p');
+	var nodoText=document.createTextNode(nameListas);
+	paragraph.appendChild(nodoText);
+	newListas.appendChild(paragraph);
+	cont.appendChild(newListas);
+}
+
